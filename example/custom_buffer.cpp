ba0c5e3e03976c36cd69cb44f906f3be37c8153d
Alexandre Arsenault <aarsenault@ruizaero.com>
2017-04-29 20:52:02 -0400

    Add struct to custom buffer example

--- example/custom_buffer.cpp
+++ example/custom_buffer.cpp
@@ -1,18 +1,28 @@
 #include <ns_getopt.h>
 
 int main() {
-	const char* args[] = {"-t", "-k"};
+	struct tk {
+		bool t = false;
+		bool k = false;
+	} tk_args;
+
+	const char* args[] = {"program_name", "-t", "-k"};
 	int args_size = sizeof(args) / sizeof(char*);
 
 	std::vector<opt::argument> opt_args = {
-		{"tommy", opt::type::no_arg, [](){}, "Tommy Lee.", 't'},
-		{"kirk", opt::type::no_arg, [](){}, "James T. Kirk.", 'k'}
+		{"tommy", opt::type::no_arg, [&tk_args](){ tk_args.t = true; }, "Tommy Lee.", 't'},
+		{"kirk", opt::type::no_arg, [&tk_args](){ tk_args.k = true; }, "James T. Kirk.", 'k'}
+	};
+
+	opt::options o = {"A wonderful example.\nTalented Author\n"
+		, "More info on github.\n", opt::DEFAULT_FLAGS
+		, [](std::string&& s) { std::cout << s << std::endl; }
 	};
 
 	if(!opt::parse_arguments(args_size, args, opt_args)) {
 		return -1;
 	}
 
-	std::cout << "Parsing arguments succeeded!" << std::endl;
+	std::cout << "t = " << tk_args.t << ", k = " << tk_args.k << std::endl;
 	return 0;
 }
