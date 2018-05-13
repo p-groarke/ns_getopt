faae15d71709411c2b81351d02a833bd6808ee1b
Philippe Groarke <philippe.groarke@gmail.com>
2018-05-13 01:22:13 -0400

    More CI.

--- build.py
+++ build.py
@@ -3,5 +3,4 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="ns_getopt:shared")
     builder.run()
