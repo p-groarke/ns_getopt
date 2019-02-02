b932396b7cdb2a51a56c82772c771a3668758502
Philippe Groarke <philippe.groarke@gmail.com>
2019-02-02 14:25:54 -0500

    Revert build.py

--- build.py
+++ build.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+from conan.packager import ConanMultiPackager
 
-from bincrafters import build_template_default
 
 if __name__ == "__main__":
-	builder = build_template_default.get_builder()
-	for settings, options, env_vars, build_requires, reference in builder.items:
-		options.update({"ns_getopt:build_testing": "True"})
-	builder.run()
+    builder = ConanMultiPackager()
+    builder.add_common_builds()
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        options.update({"ns_getopt:build_testing": "True"})
+    builder.run()
